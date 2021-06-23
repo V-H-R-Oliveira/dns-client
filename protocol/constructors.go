1b1116315c5abf915144df4d23b05ac0345b8115
Vitor Oliveira <vitor.oliveira.24.24@gmail.com>
2021-06-23 00:47:09 -0300

    Add support to reverse DNS queries.

--- protocol/constructors.go
+++ protocol/constructors.go
@@ -6,28 +6,48 @@ import (
 	"encoding/json"
 	"io"
 	"log"
+	"net"
 )
 
 func NewDNSHeader() *DNSHeader {
 	return &DNSHeader{
 		ID:      generateRequestID(),
-		Flags:   RECDISERED | RECAVAILABLE,
+		Flags:   REC_DISERED | REC_AVAILABLE,
 		QDCount: 1,
 	}
 }
 
-func NewDNSQuestion(domain string) *DNSQuestion {
+func NewDNSQuestion(domain string, class uint16) *DNSQuestion {
 	return &DNSQuestion{
 		QuestionName:  EncodeDomain(domain),
-		QuestionType:  A,
+		QuestionType:  class,
 		QuestionClass: QCLASS,
 	}
 }
 
-func NewDNSQuery(domain string) *DNSQuery {
+func NewDNSQuery(domain string, class uint16) *DNSQuery {
 	return &DNSQuery{
 		Header:   NewDNSHeader(),
-		Question: NewDNSQuestion(domain),
+		Question: NewDNSQuestion(domain, class),
+	}
+}
+
+func NewDNSStringResponse(header *DNSHeader, answersLength int) *DNSStringResponse {
+	return &DNSStringResponse{
+		Header:  header,
+		Answers: make([]*DNSStringAnswer, answersLength),
+	}
+}
+
+func NewDNSStringAnswer(name string, resourceType, class uint16,
+	ttl uint32, length uint16, data string) *DNSStringAnswer {
+	return &DNSStringAnswer{
+		Name:   name,
+		Type:   resourceType,
+		Class:  class,
+		TTL:    ttl,
+		Length: length,
+		Data:   data,
 	}
 }
 
@@ -44,8 +64,26 @@ func (query *DNSQuery) SendRequest(writter io.Writer) {
 
 func (response *DNSResponse) ToJSON(writter io.Writer) {
 	encoder := json.NewEncoder(writter)
+	encodedResponse := NewDNSStringResponse(response.Header, len(response.Answers))
 
-	if err := encoder.Encode(response); err != nil {
+	for i, resource := range response.Answers {
+		data := ""
+
+		if resource.Type == PTR {
+			data = DecodeDomain(resource.Data)
+		} else if resource.Type == AAAA || resource.Type == A {
+			ip := make(net.IP, len(resource.Data))
+			copy(ip, resource.Data)
+			data = ip.String()
+		}
+
+		stringRecord := NewDNSStringAnswer(resource.Name, resource.Type,
+			resource.Class, resource.TTL, resource.Length, data)
+
+		encodedResponse.Answers[i] = stringRecord
+	}
+
+	if err := encoder.Encode(encodedResponse); err != nil {
 		log.Fatal("Error at encoding the response:", err)
 	}
 }
--- utils/utils.go
+++ utils/utils.go
@@ -4,10 +4,12 @@ import (
 	"log"
 	"net"
 	"os"
+
+	"github.com/V-H-R-Oliveira/dns-client/protocol"
 )
 
 func CreateUDPDNSSocket() (net.Conn, error) {
-	dnsServer := net.JoinHostPort(DNSADDR, DNSPORT)
+	dnsServer := net.JoinHostPort(DNS_ADDR, DNS_PORT)
 	return net.Dial("udp", dnsServer)
 }
 
@@ -19,4 +21,15 @@ func GetInputDomains() []string {
 	}
 
 	return cmdArgs[1:]
+}
+
+func ReverseIPV4(ip net.IP) string {
+	reversedIp := net.IP{}
+
+	for i := 0; i < 4; i++ {
+		octet := ip[len(ip)-1-i]
+		reversedIp = append(reversedIp, octet)
+	}
+
+	return reversedIp.To4().String() + protocol.REVERSE_DNS_DOMAIN
 }
